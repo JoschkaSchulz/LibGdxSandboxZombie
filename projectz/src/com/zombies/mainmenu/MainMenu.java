3d0e8c561d43182b6bfb8080809120792f4f54f7
J. Schulz <Horstyschulz@gmail.com>
2013-09-27 14:31:53 +0200

    mouse and touch gestures are added to the menu

--- projectz/src/com/zombies/mainmenu/MainMenu.java
+++ projectz/src/com/zombies/mainmenu/MainMenu.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.zombies.helper.GUIHelper;
@@ -33,16 +34,19 @@ public class MainMenu extends Group {
 		Texture texButtonStart = new Texture(Gdx.files.internal("data/gfx/MainMenu/button_start.png"));
 		imgButtonStart = new Image(texButtonStart);
 		imgButtonStart.setPosition(450, GUIHelper.getNewCoordinates(60, 64));
+		imgButtonStart.setName("button_start");
 		this.addActor(imgButtonStart);
 		
 		Texture texButtonOptions = new Texture(Gdx.files.internal("data/gfx/MainMenu/button_options.png"));
 		imgButtonOptions = new Image(texButtonOptions);
 		imgButtonOptions.setPosition(340, GUIHelper.getNewCoordinates(215, 64));
+		imgButtonOptions.setName("button_options");
 		this.addActor(imgButtonOptions);
 		
 		Texture texButtonExit = new Texture(Gdx.files.internal("data/gfx/MainMenu/button_exit.png"));
 		imgButtonExit = new Image(texButtonExit);
 		imgButtonExit.setPosition(40, GUIHelper.getNewCoordinates(345, 64));
+		imgButtonExit.setName("button_exit");
 		this.addActor(imgButtonExit);
 		
 		this.debugRenderer = new ShapeRenderer();
@@ -57,6 +61,22 @@ public class MainMenu extends Group {
 	public void act(float delta) {
 		super.act(delta);
 		
+		//Mouse and Touch inputs
+		Actor overActor = this.hit(Gdx.input.getX(),Gdx.graphics.getHeight() - Gdx.input.getY(), false);
+		if(overActor != null) {
+			if (overActor.getName().equals("button_start")) {
+				selection = MENU_START;
+				if(Gdx.input.isTouched()) fireSelection();
+			}else if (overActor.getName().equals("button_options")) {
+				selection = MENU_OPTIONS;
+				if(Gdx.input.isTouched()) fireSelection();
+			}else if (overActor.getName().equals("button_exit")) {
+				selection = MENU_EXIT;
+				if(Gdx.input.isTouched()) fireSelection();
+			}
+		}
+		
+		//Keyboard inputs and maybe Controller?
 		time += delta;
 		if(InputHelper.UP && time > 0.25f) {
 			if(selection < MENU_START) {
@@ -73,19 +93,23 @@ public class MainMenu extends Group {
 			}
 			time = 0;
 		}else if(InputHelper.ACTION) {
-			switch(selection) {
-				default: 
-					projectZRef.startGame();
-				case MENU_OPTIONS:
-					break;
-				case MENU_EXIT:
-					Gdx.app.exit();
-					break;
-			}
+			fireSelection();
 		}
 		
 	}
 
+	private void fireSelection() {
+		switch(selection) {
+			default: 
+				projectZRef.startGame();
+			case MENU_OPTIONS:
+				break;
+			case MENU_EXIT:
+				Gdx.app.exit();
+				break;
+		}
+	}
+	
 	@Override
 	public void draw(SpriteBatch batch, float parentAlpha) {
 		super.draw(batch, parentAlpha);
