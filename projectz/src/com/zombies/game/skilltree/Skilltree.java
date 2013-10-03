2464e070097d2ec643e772d47526f49ef3cf6b8b
J. Schulz <Horstyschulz@gmail.com>
2013-10-03 22:02:34 +0200

    added the possibility to fill a charakter Skilltree with only the charakter skill xml

--- projectz/src/com/zombies/game/skilltree/Skilltree.java
+++ projectz/src/com/zombies/game/skilltree/Skilltree.java
@@ -1,20 +1,42 @@
 package com.zombies.game.skilltree;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.XmlReader;
 
 
 public class Skilltree {
 	private LinkedList<Skill> avaibleSkills;
-	private LinkedList<Skill> SkilltreeSkills;
+	private LinkedList<Skill> skilltreeSkills;
 	
-	public Skilltree() {
-		
+	public Skilltree(XmlReader.Element skills) {
+		this.fillSkilltree(skills);
 	}
 	
-	public void fillSkilltree(LinkedList<Skills>) {
+	/**
+	 * Creates a Skilltree out of an XML Element
+	 * 
+	 * @param skills the XML element from the charakter XML file
+	 */
+	public void fillSkilltree(XmlReader.Element skills) {
+		LinkedList<Skill> allSkills = Skill.loadSkills(Gdx.files.internal("data/xml/skills.xml"));
 		
+		Iterator iterator_skills = skills.getChildrenByName("skill").iterator();
+		while(iterator_skills.hasNext()){
+		     XmlReader.Element skill_element = (XmlReader.Element)iterator_skills.next();
+		     
+		     int id = skill_element.getIntAttribute("id");
+		     boolean known = skill_element.getBooleanAttribute("known");
+			 
+		     for(Skill s : allSkills) {
+		    	 if(s.getId() == id) {
+		    		 skilltreeSkills.add(s);
+		    		 if(known) avaibleSkills.add(s);
+		    	 }
+		     }
+		 }
 	}
 	
 	public void learnSkill(Skill skill) {
@@ -22,7 +44,7 @@ public class Skilltree {
 	}
 	
 	public void learnSkill(int skillID) {
-		for(Skill s : SkilltreeSkills) {
+		for(Skill s : skilltreeSkills) {
 			if(s.getId() == skillID) avaibleSkills.add(s);
 		}
 	}
