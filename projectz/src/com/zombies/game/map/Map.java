c2e6f47f0070305dfa450291d6937ea00d13e31c
J. Schulz <Horstyschulz@gmail.com>
2013-10-16 20:00:43 +0200

    removed map to insert the new idea

--- projectz/src/com/zombies/game/map/Map.java
+++ projectz/src/com/zombies/game/map/Map.java
@@ -20,53 +20,13 @@ public class Map extends Group {
 	public static final int TYPE_CITY 		= 0;
 	public static final int TYPE_FOREST 	= 1;
 	
-	private LinkedList<String> files[];
-	
-	private LinkedList<MapNode> nodes;
-	private Image cityBackground;
-	private float scaleFactor;
-	
 	private float dragX;
 	private float dragY;
 	
 	@SuppressWarnings("unchecked")
 	public Map() {
-		//TODO: dynamischer reader sollte eingebaut werden der jede xml in maps findet
-		this.files = new LinkedList[2];
-		this.files[TYPE_CITY] = new LinkedList<String>();		
-		this.files[TYPE_FOREST] = new LinkedList<String>();
-		
-		this.files[TYPE_CITY].add("testing_city.xml");
-		
 		dragX = 0;
 		dragY = 0;
-	}
-	
-	public void scale(int scaleFactor) {
-		cityBackground.scale(scaleFactor);
-		this.scaleFactor = scaleFactor;
-	}
-	
-	//TODO: alles  noch nicht fertig
-	private void readXML(FileHandle xmlFile) {
-		try {
-			LinkedList<Skill> skilllist = new LinkedList<Skill>();
-			
-			XmlReader xml = new XmlReader();
-			XmlReader.Element xml_element = xml.parse(xmlFile);
-			Iterator iterator_skills = xml_element.getChildrenByName("map").iterator();
-			while(iterator_skills.hasNext()){
-			     XmlReader.Element skill_element = (XmlReader.Element)iterator_skills.next();
-			     
-			     int id = skill_element.getIntAttribute("id");
-				 String name = skill_element.getChildByName("name").getText();
-				 String description = skill_element.getChildByName("description").getText();
-			     skilllist.add(new Skill(id, name, description));
-			 }
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.err.println("Fehler beim Laden der Map!");
-		}
 	}
 	
 	public void generateMap(int type) {
@@ -74,14 +34,6 @@ public class Map extends Group {
 		
 		switch(type) {
 			default:	//city
-				int rand = (int) (Math.random()*(files[TYPE_CITY].size()-1));
-				readXML(Gdx.files.internal("data/maps" + files[TYPE_CITY].get(rand)));
-				
-				Texture cityTex = new Texture(Gdx.files.internal("data/gfx/maps/testing_city.png"));
-				cityBackground = new Image(cityTex);
-				cityBackground.setPosition(0, GUIHelper.getNewCoordinates(0, 2048));
-				cityBackground.setName("city");
-				this.addActor(cityBackground);
 				break;
 			case TYPE_FOREST:
 				break;
