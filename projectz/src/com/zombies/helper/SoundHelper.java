475825db01e0c3523592dc082cfc85fd5d1ff3a0
Frederik Klauß <riki90@gmx.de>
2013-10-22 23:34:09 +0200

    MARSHMALLOWS!
    Gosia added Marshmallows to food,
    Also Starting stub of Soundhelper

---
+++ projectz/src/com/zombies/helper/SoundHelper.java
@@ -0,0 +1,11 @@
+package com.zombies.helper;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
+public class SoundHelper {
+	Sound sound = Gdx.audio.newSound(Gdx.files.internal("data/sfx/mysound.mp3"));
+	
+	
+	
+}
