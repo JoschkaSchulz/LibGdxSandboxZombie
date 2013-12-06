574fa4b8ff73ecae67bf94cc6cade050bfa4036f
J. Schulz <Horstyschulz@gmail.com>
2013-12-06 21:40:47 +0100

    refactoring the ItemHelper and added function to load items from xml

--- projectz/src/com/zombies/helper/ItemHelper.java
+++ projectz/src/com/zombies/helper/ItemHelper.java
@@ -1,39 +1,52 @@
 package com.zombies.helper;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.XmlReader;
 import com.zombies.game.inventory.*;
+import com.zombies.game.skilltree.Skill;
 
 public class ItemHelper {
 	
-private static HashMap<Integer , Consumable> Consumables;
-
-public static void initialize_all_Items(){
-	//TODO
-	//reads every item from the xml and initializes it in the specific list
-}
-
-public static Consumable createConsumable(int ID) throws InvalidIDException
-{
-	if (Consumables.containsKey(ID))
-	{
-		return (Consumable) Consumables.get(ID).clone();
-	}
-	else
-	throw new InvalidIDException("ID is not found in Consumables");
-			
-}
-
-}
-
-class InvalidIDException extends Exception
-{
-	public InvalidIDException()
-	{
+	public static HashMap<Integer , Consumable> CONSUMABLES;
 	
-	}
-	public InvalidIDException(String message)
-	{
-		super(message);
+	/**
+	 * Reads all the xml files from the items xml file and orders every
+	 * group in a seperate HashMap
+	 */
+	public static void initializeAllItems(){
+		//Initialize the static fields
+		CONSUMABLES = new HashMap<Integer, Consumable>();
+		
+		//Fill the static with all consumables
+		Consumable consumable; // = new Consumable(ID, name, discription, food, drink, health, texture)
+		int id, food, drink, health;
+		String name, description;
+	
+		try {
+			XmlReader xml = new XmlReader();
+			XmlReader.Element xml_element = xml.parse(Gdx.files.internal("data/xml/items_ger.xml"));
+			Iterator iterator_skills = xml_element.getChildByName("consumables").getChildrenByName("consumable").iterator();
+			while(iterator_skills.hasNext()){
+			     XmlReader.Element consumable_element = (XmlReader.Element)iterator_skills.next();
+			     
+			     id = consumable_element.getIntAttribute("id");
+			     name = consumable_element.getChildByName("name").getText();
+			     description = consumable_element.getChildByName("description").getText();
+			     food = consumable_element.getChildByName("effects").getChildByName("hunger").getIntAttribute("val");
+			     drink = consumable_element.getChildByName("effects").getChildByName("thirst").getIntAttribute("val");
+			     health = consumable_element.getChildByName("effects").getChildByName("health").getIntAttribute("val");
+			     
+			     consumable = new Consumable(id, name, description, food, drink, health, null);
+			     
+			     CONSUMABLES.put(id, consumable);
+			 }
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.err.println("Fehler beim Laden der XML Datei!");
+		}
 	}
 }
