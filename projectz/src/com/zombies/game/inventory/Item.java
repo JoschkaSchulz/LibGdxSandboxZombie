520623301b28c2e5a29cd2ac6c479bbc33f56e85
J. Schulz <Horstyschulz@gmail.com>
2013-12-06 21:41:02 +0100

    refactoring Item class

--- projectz/src/com/zombies/game/inventory/Item.java
+++ projectz/src/com/zombies/game/inventory/Item.java
@@ -1,43 +1,84 @@
 package com.zombies.game.inventory;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+/**
+ * An abstract class for every group of items.
+ */
 public abstract class Item {
-	private int ID;
-	private String Name;
-	private String Discription;
-	private Texture Texture;
+	/*********************************************
+	 * 			fields
+	 *********************************************/
 	
+	private int id;
+	private String name;
+	private String description;
+	private TextureRegion texture;
 	
-	public abstract Item clone();
+	/*********************************************
+	 * 			getter and setter
+	 *********************************************/
 	
-	
-	
-	public Texture getTexture() {
-		return Texture;
+	public Item(int id, String name, String description, TextureRegion texture) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.texture = texture;
 	}
-	public void setTexture(Texture texture) {
-		Texture = texture;
+	
+	/*********************************************
+	 * 			getter and setter
+	 *********************************************/
+	
+	public TextureRegion getTexture() {
+		return texture;
 	}
-	public int getID() {
-		return ID;
+	
+	public void setTexture(TextureRegion texture) {
+		this.texture = texture;
 	}
-	public void setID(int iD) {
-		ID = iD;
+	
+	public int getId() {
+		return id;
 	}
+	
+	public void setId(int iD) {
+		this.id = iD;
+	}
+	
 	public String getName() {
-		return Name;
+		return name;
 	}
+	
 	public void setName(String name) {
-		Name = name;
+		this.name = name;
 	}
+	
 	public String getDiscription() {
-		return Discription;
+		return description;
 	}
+	
 	public void setDiscription(String discription) {
-		Discription = discription;
+		this.description = discription;
 	}
 	
+	/*********************************************
+	 * 			methods
+	 *********************************************/
 	
+	/**
+	 * generates a String to let the class easier print to the console
+	 *
+	 * @return returns a String with information that is saved in this class
+	 */
+	public String toString() {
+		return "Item[id:" + getId() + ", name:" + getName() + "]";
+	}
 	
+	/**
+	 * Gives back a new identical Object of the Item class.
+	 */
+	public abstract Item clone();
+
 }
